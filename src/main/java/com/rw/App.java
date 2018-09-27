8f338a74c466b819430a683f39c34e5a7692b4b3
d33p4k <deepak.coreprogrammers@gmail.com>
2018-09-27 14:43:02 -0400

    made required changes based on new spring parent version

--- src/main/java/com/rw/App.java
+++ src/main/java/com/rw/App.java
@@ -1,33 +1,14 @@
 package com.rw;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.DispatcherServlet;
 
-import com.rw.controller.HeaderController;
 
-import org.springframework.context.ApplicationContext;
-
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @ComponentScan
@@ -38,19 +19,8 @@ import org.springframework.context.ApplicationContext;
 public class App extends SpringBootServletInitializer
 {
 	
-	/*@ResponseBody
-	@RequestMapping("/")
-	public String header(){
-		return "homepage";
-		
-	}
-	*/
-	
 	public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-		/*ApplicationContext ctx=SpringApplication.run(App.class, args);
-		HeaderController headercontroller=ctx.getBean(HeaderController.class);
-		headercontroller.header();*/
     }
     
 	@Override
@@ -58,17 +28,5 @@ public class App extends SpringBootServletInitializer
 		return application.sources(App.class);
 	}
 	
-	
-	/*@Bean
-    public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
-        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
-        Map<String,String> params = new HashMap<String,String>();
-        //params.put("org.atmosphere.servlet","org.springframework.web.servlet.DispatcherServlet");
-        //params.put("contextClass","org.springframework.web.context.support.AnnotationConfigWebApplicationContext");
-        params.put("contextConfigLocation","classpath://resource//springWeb.xml");
-        registration.setInitParameters(params);
-        return registration;
-    }
-    */
     
 }
